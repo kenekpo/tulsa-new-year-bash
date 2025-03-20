cf8da3a02ab6be89c807c246acece9aba85117c2
ken <kenneth.ekpolugh@mymhc.ca>
2025-03-20 11:36:41 -0600

    on it

---
+++ tny_script.js
@@ -0,0 +1,20 @@
+/*
+   New Perspectives on HTML5 and CSS3, 8th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Ken
+   Date: 03/19/2025 
+"use strict";
+
+*/
+/* Display the current date and time */
+document.getElementById("dateNow").innerHTML =
+"m/d/y<br />h:m:s";
+
+/* Display the time left until New Year's Eve */
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
